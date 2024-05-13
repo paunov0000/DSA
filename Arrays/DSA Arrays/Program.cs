cfabd2b77f33a3fcdf2d9f09e581375ce1cfab12
paunov0000 <legendarygriefer96@gmail.com>
2024-05-13 22:46:19 +0300

    Implemented an algorithm to find the lowest value within an array

---
+++ Arrays/DSA Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿var arr = new int[] { 83, 2, 1, -38, 5 };
+Console.WriteLine($"Lowest value is: {FindLowestValue(arr)}");
+
+
+static int FindLowestValue(int[] arr)
+{
+    //Time complexity: O(n)
+    //Space complexity: O(1)
+    var lowest = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < lowest)
+        {
+            lowest = arr[i];
+        }
+    }
+
+    return lowest;
+}
