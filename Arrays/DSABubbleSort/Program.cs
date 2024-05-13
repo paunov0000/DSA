56b2e52347d632694148bf77e0ca320d6f85fd86
paunov0000 <legendarygriefer96@gmail.com>
2024-05-14 00:22:36 +0300

    Implemented Bubble Sort

---
+++ Arrays/DSABubbleSort/Program.cs
@@ -0,0 +1,24 @@
+﻿var arr = new int[] { 3, 27, 1, 18, 7, 22, 83 };
+Console.WriteLine($"Initial array: {string.Join(", ", arr)}");
+Console.WriteLine($"Sorted array: {string.Join(", ", BubbleSort(arr))}");
+
+static int[] BubbleSort(int[] arr)
+{
+    //Time complexity: O(n^2)
+    //Space complexity: O(1)
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = 0; j < arr.Length - 1 - i; j++)
+        {
+            int tempVal;
+            if (arr[j] > arr[j + 1])
+            {
+                tempVal = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tempVal;
+            }
+        }
+    }
+
+    return arr;
+}
