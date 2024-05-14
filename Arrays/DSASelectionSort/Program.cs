1176688bab54f383f4c7646b4e3b66362c5754a5
paunov0000 <legendarygriefer96@gmail.com>
2024-05-14 16:43:41 +0300

    Selection Sort Algorithm implemented

--- Arrays/DSASelectionSort/Program.cs
+++ Arrays/DSASelectionSort/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var arr = new List<int>() { 64, 34, 25, 5, 22, 11, 90, 12 };
+
+Console.WriteLine($"Initial arr: {string.Join(", ", arr)}");
+Console.WriteLine($"Selection Sorted arr: {string.Join(", ", SelectionSort(arr))}");
+
+static List<int> SelectionSort(List<int> arr)
+{
+    int lowestValAtIndex;
+
+    for (global::System.Int32 i = 0; i < arr.Count - 1; i++)
+    {
+        lowestValAtIndex = i;
+        for (global::System.Int32 j = i + 1; j < arr.Count; j++)
+        {
+            if (arr[j] < arr[lowestValAtIndex])
+            {
+                lowestValAtIndex = j;
+            }
+        }
+
+        if (lowestValAtIndex != i)
+        {
+            var lowestVal = arr[lowestValAtIndex];
+            arr.RemoveAt(lowestValAtIndex);
+            arr.Insert(i, lowestVal);
+        }
+    }
+
+    return arr;
+}
