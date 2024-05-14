a92a7e6a0dbafc2d655341478a3d263cb77d0f39
paunov0000 <legendarygriefer96@gmail.com>
2024-05-14 17:05:43 +0300

    Implemented an improved version of the Selection Sort algorithm. The algo just swaps the val at index 'i' with 'currLowestIndex'

--- Arrays/DSASelectionSort/Program.cs
+++ Arrays/DSASelectionSort/Program.cs
@@ -1,7 +1,10 @@
 ﻿var arr = new List<int>() { 64, 34, 25, 5, 22, 11, 90, 12 };
+var arr2 = new int[] { 64, 34, 25, 5, 22, 11, 90, 12 };
 
 Console.WriteLine($"Initial arr: {string.Join(", ", arr)}");
 Console.WriteLine($"Selection Sorted arr: {string.Join(", ", SelectionSort(arr))}");
+Console.WriteLine($"Improved Selection Sorted arr: {string.Join(", ", SelectionSortImproved(arr2))}");
+
 
 static List<int> SelectionSort(List<int> arr)
 {
@@ -23,6 +26,32 @@ static List<int> SelectionSort(List<int> arr)
             var lowestVal = arr[lowestValAtIndex];
             arr.RemoveAt(lowestValAtIndex);
             arr.Insert(i, lowestVal);
+        }
+    }
+
+    return arr;
+}
+
+static int[] SelectionSortImproved(int[] arr)
+{
+    int currLowestIndex;
+
+    for (global::System.Int32 i = 0; i < arr.Length - 1; i++)
+    {
+        currLowestIndex = i;
+        for (global::System.Int32 j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[currLowestIndex])
+            {
+                currLowestIndex = j;
+            }
+        }
+
+        if (currLowestIndex != i)
+        {
+            var tempElementHolder = arr[i];
+            arr[i] = arr[currLowestIndex];
+            arr[currLowestIndex] = tempElementHolder;
         }
     }
 
